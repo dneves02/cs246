e459e602bc6c1b6775473de1f9d1f02f144d19c7
dneves02 <71541305+dneves02@users.noreply.github.com>
2020-10-17 20:13:44 -0700

    Add files via upload

---
+++ week04/HelloFromThomas/src/weaver/Main.java
@@ -0,0 +1,11 @@
+package weaver;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String myName = "Thomas";
+        String myFriend = "Davi";
+        System.out.println("Hello, my name is " + myName);
+        System.out.println("Hi, this is your friend " + myFriend);
+    }
+}
---
+++ week04/src/dneves/HelloFromDavi.java
@@ -0,0 +1,9 @@
+package dneves;
+
+public class HelloFromDavi {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, my name is Davi.");
+    }
+}
